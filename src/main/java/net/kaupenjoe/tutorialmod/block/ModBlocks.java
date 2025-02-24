9d39f557d09e424b3699af59e1b633a56f912694
JoeFrumenti <joefrumenti@gmail.com>
2025-02-23 19:21:17 -0800

    added garnets

---
+++ src/main/java/net/kaupenjoe/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,9 @@
+package net.kaupenjoe.tutorialmod.block;
+
+import net.kaupenjoe.tutorialmod.TutorialMod;
+
+public class ModBlocks {
+    public static void registerModBlocks(){
+        TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
+    }
+}
