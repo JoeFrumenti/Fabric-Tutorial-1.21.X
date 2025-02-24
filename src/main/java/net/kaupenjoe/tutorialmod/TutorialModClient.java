32e4238997a141db1bdfd353fc43e538dba883cb
JoeFrumenti <joefrumenti@gmail.com>
2025-02-23 18:12:12 -0800

    test commit

--- src/main/java/net/kaupenjoe/tutorialmod/TutorialModClient.java
+++ src/main/java/net/kaupenjoe/tutorialmod/TutorialModClient.java
@@ -1,7 +1,7 @@
 package net.kaupenjoe.tutorialmod;
 
 import net.fabricmc.api.ClientModInitializer;
-
+//aaaaaa
 public class TutorialModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
